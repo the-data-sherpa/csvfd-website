030b76df1809df5b0a65eddb7634e5f101c8596c
Christopher Owen <owen.christopher.m@gmail.com>
2025-02-24 03:14:17 -0500

    aa

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/csvfd-website/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
